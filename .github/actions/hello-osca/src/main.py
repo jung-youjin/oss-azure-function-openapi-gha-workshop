0f0ecd74a4fa6ef6e2685ee0719e6293ee11af47
YouJin Jung <ysh2491@naver.com>
2021-08-18 18:10:46 +0900

    Modify action names

--- .github/actions/hello-osca/src/main.py
+++ .github/actions/hello-osca/src/main.py
@@ -5,4 +5,4 @@ getEnvLeader = os.environ.get("INPUT_LEADER")
 getEnvMember = os.environ.get("INPUT_MEMBER")
 
 print("Hello " + getEnvLeader)
-print("Hello " + getEnvLeader)
+print("Hello " + getEnvMember)
